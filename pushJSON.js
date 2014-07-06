0ae6a9f81bb5f421fd57bf832248d438ad4cd8cc
matt <matt@theswoon.com>
2014-07-07 00:15:56 +0100

    Flatten JSON iteration

--- pushJSON.js
+++ pushJSON.js
@@ -2,17 +2,6 @@
  * Serialize an object to JSON by pushing it to a writeable stream in little bits
  */
 
-var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
-var meta = {    // table of character substitutions
-		'\b': '\\b',
-		'\t': '\\t',
-		'\n': '\\n',
-		'\f': '\\f',
-		'\r': '\\r',
-		'"' : '\\"',
-		'\\': '\\\\'
-};
-
 module.exports = function(out,obj,replacer,onComplete) {
 //	The stringify method takes a value and an optional replacer, and an optional
 //	space parameter, and returns a JSON text. The replacer can be a function
@@ -21,86 +10,99 @@ module.exports = function(out,obj,replacer,onComplete) {
 //	produce text that is more easily readable.
 	var enc = "utf8";
 
+	if (obj===undefined) 
+		return "" ;
 //	If there is a replacer, it must be a function or an array.
 //	Otherwise, throw an error.
-	var rep = replacer;
-	if (rep==null) rep = function(k,v){return v;}
-	else if (typeof rep === 'function'){}
-	else if (Array.isArray(rep)) {
-		rep = function() {
-			// Do somethng with replacer
-		}
-	} else throw new Error("Replacer must be nullish, a function or an Array") ;
 
-	function xform(parent,k,o) {
+	var xform ;
+	if (replacer==null) {
+		xform = function(parent,k,o) {
+			if (o && typeof o === 'object' && typeof o.toJSON === 'function') {
+				o = value.toJSON(k);
+			}
+			return o ;
+		}
+	}else if (typeof replacer === 'function'){
+		xform = function(parent,k,o) {
+			if (o && typeof o === 'object' && typeof o.toJSON === 'function') {
+				o = value.toJSON(k);
+			}
+			return replacer.call(parent,k,o) ;
+		}
+	} else if (Array.isArray(replacer)) {
 		if (o && typeof o === 'object' && typeof o.toJSON === 'function') {
 			o = value.toJSON(k);
 		}
-		return rep.call(parent,k,o) ;
-	}
-	
+		// Do something with replacer
+		//return replacer.call(parent,k,o) ;
+		return o ;
+	} else throw new Error("Replacer must be nullish, a function or an Array") ;
+
 	var stack = [] ;
-	var next = null ;
+	var next = function(){ onComplete() };
 	var index = 0 ;
-	obj = {'':obj} ;
+	var sep ;
 	
-	var yield = 0 ;
-	function write(obj,enc,next) {
-		yield += 1 ;
-		if (out.write(obj,enc||"utf8")) {
-			if (next) {
-				if (yield%100)
-					next() ;
-				else
-					setImmediate(next) ;
-			} 
-		} else { 
-			next && out.once('drain',next) ;
+	function iter(fn) {
+		return function() {
+			while (fn = fn()) ;
 		}
 	}
-	
-	function walk(parent,k) {
-		obj = xform(parent,k,obj) ;
-		if (obj==undefined) {
-			next() ;
-		} else if (obj==null) {
-			write('null',enc,next);
-		} else if (typeof obj === "string") {
-			write(quote(obj),enc,next);
-		} else if (typeof obj === "number") {
-			write(isFinite(obj) ? String(obj) : 'null',enc,next);
-		} else if (typeof obj === "boolean" || typeof obj === "null") {
-			write(String(obj),enc,next);
+
+	var yield = 0 ;
+	function write(data,encoding,done) {
+		if (done) {
+			yield += 1 ;
+			if (out.write(data,encoding)) {
+				//if (yield&0x0FFF)
+					return done ;
+				//setImmediate(iter(done)) ;
+				return ;
+			}
+			out.once('drain',iter(done)) ;
+			return ;
+		}
+		out.write(data,encoding) ;
+		return ;
+	}
+
+	function walk() {
+		if (obj==null) {
+			return write('null',enc,next);
+		} else if (typeof obj !== "object" || obj instanceof Number) {
+			//return write(JSON.stringify(obj),enc,next);
+			return write(quote(obj.toString()),enc,next);
 		} else if (Array.isArray(obj)) {
 			if (obj.length==0)
-				write('[]',enc,next);
+				return write('[]',enc,next);
 			else {
 				// Save state
 				stack.push({next:next,index:index,obj:obj}) ;
-				
-				write('[',enc);
-				
+
 				// Current obj
 				index = 0 ;
-				var p = obj ;
-				obj = obj[index] ;
-				next = function() {
-					index += 1 ;
+				function stepArray() {
 					var peek = stack[stack.length-1] ;
 					if (index<peek.obj.length) {
-						write(',');
 						obj = peek.obj[index] ;
-						return walk(peek.obj,index) ;
+						obj = xform(peek.obj,index,obj) ;
+						index += 1 ;
+						write(sep);
+						sep = ", " ;
+						return walk ;
 					} else {
 						write(']');
-						pop = stack.pop() ;
+						var pop = stack.pop() ;
 						obj = pop.obj ;
 						index = pop.index ;
 						next = pop.next ;
-						return next && next() ;
+						return next ;
 					}
 				}
-				return walk(p,index) ;
+				sep = "[" ;
+				next = stepArray ;
+				return next ;
 			}
 		} else {
 			var keys = Object.keys(obj) ;
@@ -110,38 +112,49 @@ module.exports = function(out,obj,replacer,onComplete) {
 			obj = {keys:keys,v:obj} ;
 			// Save state
 			stack.push({next:next,index:index,obj:obj}) ;
-			
+
 			// Current obj
 			index = 0 ;
-			if (obj.keys[index]!=="")
-				write("{"+quote(obj.keys[index])+":",enc);
-			var p = obj.v ;
-			var k = obj.keys[index] ;
-			obj = obj.v[k] ;
-			next = function() {
-				index += 1 ;
+			function stepObject() {
 				var peek = stack[stack.length-1] ;
 				if (index<peek.obj.keys.length) {
-					write(", "+quote(peek.obj.keys[index])+":",enc);
-					obj = peek.obj.v[peek.obj.keys[index]] ;
-					return walk(peek.obj.v,peek.obj.keys[index]) ;
+					var k2 = peek.obj.keys[index] ;
+					obj = peek.obj.v[k2] ;
+					obj = xform(peek.obj.v,k2,obj) ;
+					index += 1 ;
+					if (obj===undefined) {
+						return next ;
+					}
+					write(sep+quote(k2)+":",enc);
+					sep = ", " ;
+					return walk ;
 				} else {
-					pop = stack.pop() ;
+					var pop = stack.pop() ;
 					obj = pop.obj ;
 					index = pop.index ;
 					next = pop.next ;
-					if (next) {
-						write('}');
-						return next() ;
-					}
-					return onComplete && onComplete() ;
+					write('}');
+					return next ;
 				}
 			}
-			return walk(p,k) ;
+			sep = "{" ;
+			next = stepObject ;
+			return next ;
 		}
 	}
-	debugger; 
-	walk(obj,'') ;
+	obj = xform(null,'',obj) ;
+	iter(walk)() ;
+};
+
+var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+var meta = {    // table of character substitutions
+		'\b': '\\b',
+		'\t': '\\t',
+		'\n': '\\n',
+		'\f': '\\f',
+		'\r': '\\r',
+		'"' : '\\"',
+		'\\': '\\\\'
 };
 
 function quote(string) {
--- test/json.js
+++ test/json.js
@@ -1,3 +1,73 @@
 var json = require('../pushJSON') ;
 
-json(process.stdout,[123,"abc",{name:"xyz",age:987},456],null,null) ;
+var $error = console.log.bind(console) ;
+DEBUG = $error ; 
+var http = require('nodent')({use:['https']}).https ;
+var URL = require('url') ;
+
+var neo4j = require('swoon-neo4j') ;;
+
+var devnull = {
+		write:function(data,enc,done){ 
+			done && process.nextTick(done); 
+			return true;
+		}
+};
+
+//V3 initialisation
+
+function report() {
+	for (var i=2; i<t.length; i+=2)
+		console.log(t[i]+":\t"+(t[i+1]-t[i-1])) ;
+}
+var t = ["start",Date.now()] ;
+/*
+neo4j.start({neo4jURI:"http://dev.favr.tt:7474"},"connect",{
+	vmOptions:null,
+	classpath:null,
+	databaseProperties:null,
+	serverProperties:null,
+	haProperties:null
+})(function(db){
+	for (var n=0; n<1; n++) {
+		db.cypher("match (u:User)-->(o:Offer) where has(u.id) return distinct o,count(u)")
+		(function(d){
+			t.push("rows "+d.length) ;
+			t.push(Date.now()) ;
+			json(devnull,d,null,function(){
+				t.push("async") ;
+				t.push(Date.now()) ;
+				devnull.write(JSON.stringify(d),"utf8",function(){
+					t.push("JSON") ;
+					t.push(Date.now()) ;
+					report() ;
+				}) ;
+			}) ;
+		},$error);
+		t.push("start") ;
+		t.push(Date.now()) ;
+	}
+},$error) ;
+ */
+/*json(process.stdout,[123,"abc",{name:"xyz",age:987},456],null,function(){
+	t.push("\nJSON") ;
+	t.push(Date.now()) ;
+	report() ;
+}) ;*/
+
+var x = URL.parse("https://api.github.com/repos/joyent/node/issues?state=all&since=2000-01-01Z00:00:00") ;
+x.headers = {'User-Agent':"Nodent", 'Accept':'application/json'} ;
+http.getBody(x)(function(body){
+	var d = JSON.parse(body) ;
+	t.push("http:"+d.length) ;
+	t.push(Date.now()) ;
+	json(devnull,d,null,function(){
+		t.push("async") ;
+		t.push(Date.now()) ;
+		devnull.write(JSON.stringify(d),"utf8",function(){
+			t.push("JSON") ;
+			t.push(Date.now()) ;
+			report() ;
+		}) ;
+	}) ;
+},$error) ;
