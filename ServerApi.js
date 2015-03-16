3affcf716826bad4cd5e9cb03be9c59216ebeb7b
Matthew W <matthew.woolf@mailonline.co.uk>
2015-03-16 09:57:53 +0000

    Fix parsing of Content-Type; charset in ServerApi. Bump Nodent version

--- ServerApi.js
+++ ServerApi.js
@@ -30,15 +30,17 @@ function callRemoteFuncBack(that,path,args) {
 			var body = "" ;
 			res.on('data', function (chunk) { body += chunk ; });
 			res.once('end',function(){
+				var contentType = x.getResponseHeader("Content-Type") ;
+				if (contentType) contentType = contentType.split(";")[0] ; 
 				if (res.statusCode==200) {
 					DEBUG(1,path,args,res.statusCode,(Date.now()-tStart)+"ms") ;
 					var data = body ;
-					if (res.headers['content-type'].split(";")[0]=="application/json")
+					if (contentType=="application/json")
 						data = !data?data:JSON.parse(data,that.reviver) ;
 					callback(data) ;
 				} else {
 					DEBUG(25,path,args,res.statusCode,(Date.now()-tStart)+"ms\n"+body) ;
-					if (res.headers['content-type'].split(";")[0]=="application/json") {
+					if (contentType=="application/json") {
 						var exception = JSON.parse(body,that.reviver) ;
 						var exc = new Error(body) ;
 						Object.defineProperties(exc, getOwnPropertyDescriptions(exception)) ;
