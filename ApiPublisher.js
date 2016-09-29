0ad94cb31ae1f5ff7c47a872bb5637ee66384eb8
matAtWork <matthew.woolf@mailonline.co.uk>
2016-09-29 08:11:41 +0100

    Remove chaff

--- ApiPublisher.js
+++ ApiPublisher.js
@@ -8,7 +8,6 @@
 
 var nodent = require('nodent')({dontInstallRequireHook:true}) ;
 var map = nodent.require('map') ;
-var DEBUG = global.DEBUG || (process.env.DEV ? function(l){ process.env.DEV<=l && console.log.apply(this,arguments); }:function(){}) ;
 var Thenable = global.Promise || nodent.EagerThenable() ;
 
 /**
@@ -17,45 +16,48 @@ var Thenable = global.Promise || nodent.EagerThenable() ;
  * @param obj	- the object containing the functions to make available remotely
  */
 function ApiPublisher(obj) {
-	var that = this ;
-	that.api = {} ;
-	that.names = {} ;
-	that.cache = {} ;
-	that.nested = {} ;
-	that.context = obj ;
+    var that = this ;
+    that.api = {} ;
+    that.names = {} ;
+    that.cache = {} ;
+    that.nested = {} ;
+    that.context = obj ;
 
-	for (var i in obj) if (obj.hasOwnProperty(i)){
-		if (typeof obj[i] == 'function') {
-			that.api[i] = {fn:obj[i]} ;
-			that.names[i] = { parameters: obj[i].length } ; // Remote call info 
+    for (var i in obj) if (obj.hasOwnProperty(i)){
+        if (typeof obj[i] == 'function') {
+            that.api[i] = {fn:obj[i]} ;
+            that.names[i] = { parameters: obj[i].length } ; // Remote call info 
 			try {
 				that.names[i].parameters = obj[i].toString().match(/[^(]*(\(.*\))/)[1] ;
 			} catch (ex) {
-				DEBUG(40,ex) ;
+				// Unknown number of parameters
 			}
-			
-			if (obj[i].ttl)
-				that.names[i].ttl = obj[i].ttl ; // Remote call info 
-		}
-	}
-	
-	setInterval(function(){
-		var now = Date.now() ;
-		Object.keys(that.cache).forEach(function(k){
-			var j = Object.keys(that.cache[k]) ;
-			if (!j.length)
-				delete that.cache[k] ;
-			else j.forEach(function(e){
-				if (that.cache[k][e].expires < now)
-					delete that.cache[k][e] ;
-			}) ;
-		}) ;
-	},65536) ;
-	
-	that.handle = ApiPublisher.prototype.handle.bind(this) ;
-	return that ;
+            
+            if (obj[i].ttl)
+                that.names[i].ttl = obj[i].ttl ; // Remote call info 
+        }
+    }
+
+    setInterval(function(){
+        var now = Date.now() ;
+        Object.keys(that.cache).forEach(function(k){
+            var j = Object.keys(that.cache[k]) ;
+            if (!j.length)
+                delete that.cache[k] ;
+            else j.forEach(function(e){
+                if (that.cache[k][e].expires < now)
+                    delete that.cache[k][e] ;
+            }) ;
+        }) ;
+    },65536) ;
+
+    that.handle = ApiPublisher.prototype.handle.bind(this) ;
+    return that ;
 }
 
+ApiPublisher.prototype.warn = function() {
+    console.warn.apply(console,['ApiPublisher:'].concat(arguments));
+}
 
 /**
  * Return an object whose keys represent the remotely callable 
@@ -73,57 +75,57 @@ function ApiPublisher(obj) {
  * @param ok
  */
 ApiPublisher.prototype.getRemoteApi = function(req,path,ok) {
-	var self = this ;
-	if (path)
-		self.path = path ;
-	map(self.api, function(e){
-		return new Thenable(function(ok,error) {
-			var fn = self.api[e].fn ;
-			if (fn[req.apiVersion])
-				fn = fn[req.apiVersion] ;
-			if (fn.clientInstance) {
-				if (fn.length != fn.clientInstance.length) {
-					DEBUG(20,"Warning: Remote instance function arguments not the same as declaration:",e) ;
-				}
-				return fn.apply({request:req},fn.clientInstance).then(function(instanceData){
-					if (instanceData instanceof ApiPublisher) {
-						 self.nested[e] = instanceData ;
-						 instanceData.getRemoteApi(req, e, function(api){
-							 api._isRemoteApi = true ;
-							 ok(api) ;
-						 }) ;
-					} else {
-						ok(instanceData) ;
-					}
-				},error) ;
-			} else {
-				ok(self.names[e]) ;
-			}
-		});
-	}).then(ok,$error) ;
+    var self = this ;
+    if (path)
+        self.path = path ;
+    map(self.api, function(e){
+        return new Thenable(function(ok,error) {
+            var fn = self.api[e].fn ;
+            if (fn[req.apiVersion])
+                fn = fn[req.apiVersion] ;
+            if (fn.clientInstance) {
+                if (fn.length != fn.clientInstance.length)
+                    self.warn("Remote instance function arguments not the same as declaration:",e) ;
+
+                return fn.apply({request:req},fn.clientInstance).then(function(instanceData){
+                    if (instanceData instanceof ApiPublisher) {
+                        self.nested[e] = instanceData ;
+                        instanceData.getRemoteApi(req, e, function(api){
+                            api._isRemoteApi = true ;
+                            ok(api) ;
+                        }) ;
+                    } else {
+                        ok(instanceData) ;
+                    }
+                },error) ;
+            } else {
+                ok(self.names[e]) ;
+            }
+        });
+    }).then(ok,$error) ;
 };
 
 var stdHeaders = {
-	'Content-Type': 'application/json; charset=utf-8',
-	'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0',
-	'Expires': 'Fri, 01 Jan 1990 00:00:00 GMT',
-	'pragma': 'no-cache'
+    'Content-Type': 'application/json; charset=utf-8',
+    'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0',
+    'Expires': 'Fri, 01 Jan 1990 00:00:00 GMT',
+    'pragma': 'no-cache'
 } ;
 
 ApiPublisher.prototype.sendRemoteApi = function(req,rsp) {
-	var that = this ;
-	this.getRemoteApi(req,null,function(instance){
-		rsp.writeHead(200, stdHeaders);
-		rsp.end(JSON.stringify(instance,that.serializer(req,rsp)));
-	}) ;
+    var that = this ;
+    this.getRemoteApi(req,null,function(instance){
+        rsp.writeHead(200, stdHeaders);
+        rsp.end(JSON.stringify(instance,that.serializer(req,rsp)));
+    }) ;
 };
 
 function stringRepresentation(o) {
-	try {
-		return JSON.stringify(o)+o.toString() ;
-	} catch (ex) {
-		return o.toString() ;
-	}
+    try {
+        return JSON.stringify(o)+o.toString() ;
+    } catch (ex) {
+        return o.toString() ;
+    }
 }
 
 function hashCode(o) {
@@ -148,162 +150,154 @@ function hash(o) {
  * Remote invocation of a local async funcback API. 
  **/
 ApiPublisher.prototype.sendReturn = function(req,rsp,result,status) {
-	var json = JSON.stringify(result,this.serializer(req,rsp)) ;
-	rsp.end(json);
+    var json = JSON.stringify(result,this.serializer(req,rsp)) ;
+    rsp.end(json);
 }
 
 ApiPublisher.prototype.callRemoteApi = function(name,req,rsp) {
-	var tStart = Date.now() ;
-	var args = name.split("?"),that = this ;
-	var fn, cacheEntry, key ;
-	
-	name = args[0] ;
-	DEBUG(1,"ApiPublisher",name,req.socket.remotePort,that.cache) ;
-	if (!that.api[name]) {
-		return errorCB(new Error("Endpoint not found: "+name),404) ;
-	}
-	
-	// Because we like to accept nicely posted JSON, _and_ form input data, we 
-	// test the body type
-	if (req.method=="POST")
-		args = req.body; 
-	else if (req.method=="GET") {
-		if (args[1]) {
-		    try {
-	            args = JSON.parse(decodeURIComponent(args[1])) ;
-		    } catch (ex) {
-		        return errorCB(new Error("Incorrect parameter format "+req.method+" "+ex.message),500) ;
-		    }
-		} else args = [] ;
-	} else {
-		return errorCB(new Error("Method not allowed: "+req.method),405) ;
-	}
-	
-	if (!Array.isArray(args))
-		args = [args] ; // Wasn't a JSON encoded argument list, so wrap it like it was
+    var tStart = Date.now() ;
+    var args = name.split("?") ;
+    var fn, cacheEntry, key ;
 
-	// Proto-augment "this" with the current request so the remoted API can query session
-	// info etc.
-	var context = that.proxyContext(name,req,rsp,args) ;
+    name = args[0] ;
+    if (!this.api[name]) {
+        return errorCB(new Error("Endpoint not found: "+name),404) ;
+    }
 
-	// Is this result cachable on the server?
-	if (that.names[name].ttl && that.names[name].ttl.server) {
-		key = hash(that.cacheObject(context)) ;
-		if (key) {
-			that.cache[name] = that.cache[name] || {} ;
-			cacheEntry = that.cache[name][key] ;
-DEBUG(1,"Cache Entry",name,req.socket.remotePort,key,cacheEntry) ;
-			if (cacheEntry) {
-				if (cacheEntry.expires) {
-					if (cacheEntry.expires > Date.now()) {
-						return sendReturn({value:cacheEntry.data});
-					}
-					delete cacheEntry.expires ;
-					delete cacheEntry.data ;
-				}
+    // Because we like to accept nicely posted JSON, _and_ form input data, we 
+    // test the body type
+    if (req.method=="POST")
+        args = req.body; 
+    else if (req.method=="GET") {
+        if (args[1]) {
+            try {
+                args = JSON.parse(decodeURIComponent(args[1])) ;
+            } catch (ex) {
+                return errorCB(new Error("Incorrect parameter format "+req.method+" "+ex.message),500) ;
+            }
+        } else args = [] ;
+    } else {
+        return errorCB(new Error("Method not allowed: "+req.method),405) ;
+    }
 
-				if (cacheEntry.pending) {
-					// Another call is pending, and will return the data/error on completion
-					return cacheEntry.pending.push(sendReturn) ;
-				}
-			} else {
-				cacheEntry = that.cache[name][key] = {} ;
-			}
-			cacheEntry.pending = cacheEntry.pending || [] ;
-		}
-	}
+    if (!Array.isArray(args))
+        args = [args] ; // Wasn't a JSON encoded argument list, so wrap it like it was
 
-	fn = that.api[name].fn ;
-	if (fn[req.apiVersion])
-		fn = fn[req.apiVersion] ; 
-	
-	return fn.apply(context,args).then(returnCB,errorCB) ;
+    // Proto-augment "this" with the current request so the remoted API can query session
+    // info etc.
+    var context = this.proxyContext(name,req,rsp,args) ;
 
-	/* Send the response to the client */
-	function sendReturn(result){
-		if (rsp.headersSent) {
-			DEBUG(99,"Response already sent",name,args,result) ;
-		} else {
-			for (var i in stdHeaders)
-				rsp.setHeader(i,stdHeaders[i]) ;
-			rsp.statusCode = result.status || 200 ;
-			if (result.status>=500) {
-				DEBUG(28,"5xx Response: ",req.session) ;
-			}
-			DEBUG(1,name,args," "+(Date.now()-tStart)+"ms") ;
-			that.sendReturn(req,rsp,result.value,result.status) ;
-			DEBUG(1,name,args," sent "+(Date.now()-tStart)+"ms") ;
-		}
-	} ;
-	
-	/* Send the result to any pending clients that are listening */
-	function sendPending(result) {
-		var pending = cacheEntry.pending ;
-		delete cacheEntry.pending ;
-		pending && pending.forEach(function(returner,idx){
-			returner(result) ;
-		}) ;
-	}
-	
-	/* Send a successful result, updating the cache if required */
-	function returnCB(t,status) {
-		if (nodent.isThenable(t)) {
-			return t.then(returnCB,errorCB) ;
-		}
-		
-		var result = {value:t,status:status} ;
-		if (!status || status==200) { 
-			if (t instanceof ApiPublisher) {
-				t.sendRemoteApi(req,rsp) ;
-				return ;
-			}
-			if (cacheEntry) {
-				sendPending(result) ;
-				Object.defineProperty(cacheEntry,'data',{
-					value:t,
-					enumerable:false,
-					writeable:true,
-					configurable:true
-				}) ;
-				cacheEntry.expires = Date.now()+1000*that.names[name].ttl.server ;
-			}
-		} else {
-			if (cacheEntry) {
-				sendPending(result) ;
-				delete cacheEntry.data;
-				delete cacheEntry.expires;
-			}
-		}
-		sendReturn(result);
-	};
-	
-	/* Send an error result, invalidating the cache */
-	function errorCB(err,status) {
-		DEBUG(status?29:20,err,status) ;
-		if (!(err instanceof Error))
-			err = new Error(err.toString()) ;
+    // Is this result cachable on the server?
+    if (this.names[name].ttl && this.names[name].ttl.server) {
+        key = hash(this.cacheObject(context)) ;
+        if (key) {
+            this.cache[name] = this.cache[name] || {} ;
+            cacheEntry = this.cache[name][key] ;
+            if (cacheEntry) {
+                if (cacheEntry.expires) {
+                    if (cacheEntry.expires > Date.now()) {
+                        return sendReturn({value:cacheEntry.data});
+                    }
+                    delete cacheEntry.expires ;
+                    delete cacheEntry.data ;
+                }
 
-		var result = {value:{error:err.message,cause:err.stack} ,status:status || 500} ;
-		if (cacheEntry) {
-			sendPending(result) ;
-			delete cacheEntry.data;
-			delete cacheEntry.expires;
-		}
-		
-		sendReturn(result);
-	} ;
+                if (cacheEntry.pending) {
+                    // Another call is pending, and will return the data/error on completion
+                    return cacheEntry.pending.push(sendReturn) ;
+                }
+            } else {
+                cacheEntry = this.cache[name][key] = {} ;
+            }
+            cacheEntry.pending = cacheEntry.pending || [] ;
+        }
+    }
+
+    fn = this.api[name].fn ;
+    if (fn[req.apiVersion])
+        fn = fn[req.apiVersion] ; 
+
+    var that = this ;
+
+    /* Send the response to the client */
+    function sendReturn(result){
+        if (!rsp.headersSent) {
+            for (var i in stdHeaders)
+                rsp.setHeader(i,stdHeaders[i]) ;
+            rsp.statusCode = result.status || 200 ;
+            that.sendReturn(req,rsp,result.value,result.status) ;
+        }
+    } ;
+
+    /* Send the result to any pending clients that are listening */
+    function sendPending(result) {
+        var pending = cacheEntry.pending ;
+        delete cacheEntry.pending ;
+        pending && pending.forEach(function(returner,idx){
+            returner(result) ;
+        }) ;
+    }
+
+    /* Send a successful result, updating the cache if required */
+    function returnCB(t,status) {
+        if (nodent.isThenable(t)) {
+            return t.then(returnCB,errorCB) ;
+        }
+
+        var result = {value:t,status:status} ;
+        if (!status || status==200) { 
+            if (t instanceof ApiPublisher) {
+                t.sendRemoteApi(req,rsp) ;
+                return ;
+            }
+            if (cacheEntry) {
+                sendPending(result) ;
+                Object.defineProperty(cacheEntry,'data',{
+                    value:t,
+                    enumerable:false,
+                    writeable:true,
+                    configurable:true
+                }) ;
+                cacheEntry.expires = Date.now()+1000*that.names[name].ttl.server ;
+            }
+        } else {
+            if (cacheEntry) {
+                sendPending(result) ;
+                delete cacheEntry.data;
+                delete cacheEntry.expires;
+            }
+        }
+        sendReturn(result);
+    };
+
+    /* Send an error result, invalidating the cache */
+    function errorCB(err,status) {
+        if (!(err instanceof Error))
+            err = new Error(err.toString()) ;
+
+        var result = {value:{error:err.message,cause:err.stack} ,status:status || 500} ;
+        if (cacheEntry) {
+            sendPending(result) ;
+            delete cacheEntry.data;
+            delete cacheEntry.expires;
+        }
+
+        sendReturn(result);
+    } ;
+    
+    return fn.apply(context,args).then(returnCB,errorCB) ;
 };
 
 ApiPublisher.prototype.cacheObject = function(obj) {
-	return {args:obj.arguments,version:obj.request.apiVersion} ;
+    return {args:obj.arguments,version:obj.request.apiVersion} ;
 };
 
 ApiPublisher.prototype.proxyContext = function(name,req,rsp,args) {
-	return Object.create(this.context,{ request:{value:req}, arguments:{value:args} }) ;
+    return Object.create(this.context,{ request:{value:req}, arguments:{value:args} }) ;
 };
 
 ApiPublisher.prototype.serializer = function(req,rsp) {
-	return null ;
+    return null ;
 };
 
 /**
@@ -317,46 +311,46 @@ ApiPublisher.prototype.serializer = function(req,rsp) {
  */
 
 ApiPublisher.prototype.handle = function(req,rsp,next) {
-	var url = req.url.toString() ;
-	var path = url.split("/") ;
-	if (path[path.length-1]=="")
-		path.pop(); // Strip any trailing "/"
-	req.apiVersion = Number(path[path.length-1].match(/^[0-9.]+$/)) ; 
-	if (req.apiVersion>0)
-		path.pop() ; // Strip the version number
-	if (path.length<2 || path[1]=="") {
-		this.sendRemoteApi(req,rsp) ;
-	} else {
-		var self = this ;
-		path.shift() ;
-		var call = decodeURIComponent(path.pop()) ;
-		
-		(function walkPath() {
-			if (path.length===0)
-				return self.callRemoteApi(call,req,rsp) ;	// Client is making a remote call
-			
-			var e = path.shift() ;
-			var subApi = self.nested[e] ;
-			if (subApi)
-				return subApi.callRemoteApi(call,req,rsp) ;
+    var url = req.url.toString() ;
+    var path = url.split("/") ;
+    if (path[path.length-1]=="")
+        path.pop(); // Strip any trailing "/"
+    req.apiVersion = Number(path[path.length-1].match(/^[0-9.]+$/)) ; 
+    if (req.apiVersion>0)
+        path.pop() ; // Strip the version number
+    if (path.length<2 || path[1]=="") {
+        this.sendRemoteApi(req,rsp) ;
+    } else {
+        var self = this ;
+        path.shift() ;
+        var call = decodeURIComponent(path.pop()) ;
 
-			if (self.api[e] &&
-				self.api[e].fn &&
-				self.api[e].fn.clientInstance && 
-				self.api[e].fn.clientInstance.length===0) {
-				
-				return self.api[e].fn.apply({request:req}).then(function(instanceData){
-					if (instanceData instanceof ApiPublisher) {
-						 self.nested[e] = instanceData ;
-						 path.unshift(e) ;
-						 return walkPath() ;
-					} 
-					return next() ;
-				},next) ;
-			}
-			return next(/* Could not find/load nested API*/) ;
-		})() ;
-	}
+        (function walkPath() {
+            if (path.length===0)
+                return self.callRemoteApi(call,req,rsp) ;	// Client is making a remote call
+
+            var e = path.shift() ;
+            var subApi = self.nested[e] ;
+            if (subApi)
+                return subApi.callRemoteApi(call,req,rsp) ;
+
+            if (self.api[e] &&
+                self.api[e].fn &&
+                self.api[e].fn.clientInstance && 
+                self.api[e].fn.clientInstance.length===0) {
+
+                return self.api[e].fn.apply({request:req}).then(function(instanceData){
+                    if (instanceData instanceof ApiPublisher) {
+                        self.nested[e] = instanceData ;
+                        path.unshift(e) ;
+                        return walkPath() ;
+                    } 
+                    return next() ;
+                },next) ;
+            }
+            return next(/* Could not find/load nested API*/) ;
+        })() ;
+    }
 } ;
 
 module.exports = ApiPublisher;
